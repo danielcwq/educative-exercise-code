49d242b23a01cab0cc0d851ce758da1971e5ad08
danielcwq <75194564+danielcwq@users.noreply.github.com>
2020-12-22 10:41:02 +0800

    Create jspractice1.js

---
+++ jspractices/jspractice1.js
@@ -0,0 +1,13 @@
+// Write your code here
+var word1 = "Billy";
+var word2 = "went";
+var word3 = "for";
+var word4 = "a"; 
+var word5 = "walk";
+var space = " ";
+var period = ".";
+var sentence =(word1 + space + word2 + space + word3 + space+  word4 + space + word5 + period)
+console.log(sentence)
+// store the result in a sentence variable
+// print the result
+//this exercise was basically to combine strings together to form a sentence 
